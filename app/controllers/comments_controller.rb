c5fb5696111a4ade7261de42dacbbb87929fc790
Tonee Young <itisi@toneeyoung.com>
2017-09-27 22:39:26 -0400

    Fix issue where wrong code was in comment partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:post_id]
-    @comment = @post.comment.create(params[:comment].permit(:name, :body))
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:name, :body))
 
     redirect_to post_path(@post)
   end
