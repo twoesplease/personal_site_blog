ac568ddaa59172295925fb585db51d43119ddfab
Tonee Young <itisi@toneeyoung.com>
2018-01-11 14:23:39 -0500

    Update config to compile assets properly

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# required for asset compilation
+config.assets.initialize_on_precompile = false
+
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
