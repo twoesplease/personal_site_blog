05701d7fadb4f852dfe35678f27c84e5a66b9c27
Tonee Young <itisi@toneeyoung.com>
2018-02-08 14:04:24 -0500

    Remove drop users table migration, add seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+posts = Post.create(title: "test post", body: "***Here*** is a test", created_at: "2018-01-26 18:45:40", updated_at: "2018-01-31 00:14:43")
