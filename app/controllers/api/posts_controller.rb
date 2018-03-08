13b00d3af12daa4a001e9e603d97a6a846e15fc2
Tonee Young <itisi@toneeyoung.com>
2018-03-08 14:06:10 -0500

    Add update & delete API endpoints. Fix create API endpoint.

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -13,14 +13,48 @@ class Api::PostsController < ApplicationController
   def create
     post = Post.new(post_params) 
     if post.save
-      head 200
+      render json: {
+        status: 200,
+        message: "You did it! Post successfully created.",
+        post: post
+      }.to_json
     else
-      head 500
+      render json: {
+        status: 500,
+        message: "Hmm, that didn't work.  Try again.",
+        errors: post.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    render json: {
+      status: 200,
+      message: "That post has been successfully deleted."
+    }.to_json
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      render json: {
+        status: 200,
+        message: "Post successfully updated.",
+        post: post
+      }.to_json
+    else
+      render json: {
+        status: 500,
+        message: "The post could not be updated.",
+        post: post
+      }.to_json
     end
   end
 
   private
   def post_params
-    params.require("post").permit("title")
+    params.require(:post).permit(:title, :body)
   end
 end
