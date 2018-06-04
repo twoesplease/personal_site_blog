bea073d288d2c8a0d059ae56fc280277806ea3de
Tonee Young <itisi@toneeyoung.com>
2018-06-04 15:52:26 -0400

    Try fixing css issue by removing contents of public/assets and removing directive to precompile assets in app config file

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # required for asset compilation
-    config.assets.initialize_on_precompile = true
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
