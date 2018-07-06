2db1e210a5f4abcc8a8e778c50ea9912b3b4dc67
Tonee Young <itisi@toneeyoung.com>
2018-07-05 22:16:07 -0400

    Order posts by date created, descending

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
+  default_scope -> { order(created_at: :desc) }
 end
